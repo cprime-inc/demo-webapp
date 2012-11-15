20b1277697b05a915a085a93cf06fbbccff224ad
Sarah Goff-Dupont <sgoffdupont@atlassian.com>
2012-11-15 11:31:38 -0800

    Initial commit.

---
+++ src/test/TestCreateIRKD.java
@@ -0,0 +1,44 @@
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCreateIRKD {
+	
+	
+	@Test (groups = {"billing", "smoke-tests"})
+	public void testHasNerdGlasses() throws Exception {
+		/////////////////
+		// your actual test would go here
+		/////////////////
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test (groups = {"batch2", "integration-tests"})
+	public void testHasNoSocialLife() throws Exception {
+		/////////////////
+		// your actual test would go here
+		/////////////////
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test (groups = {"batch2", "integration-tests"})
+	public void testPlaysBoardGames() throws Exception {
+		/////////////////
+		// your actual test would go here
+		/////////////////
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test (groups = {"batch1", "integration-tests"})
+	public void testBelongsToChessClub() throws Exception {
+		/////////////////
+		// your actual test would go here
+		/////////////////
+		
+		Assert.assertTrue(true);
+	}
+}
