70147ea9827516deda127143b94d26969b2e687f
Sarah Goff-Dupont <sgoffdupont@atlassian.com>
2012-12-11 14:36:03 -0800

    Really, really truly the finishing touches on my refactor, per IRKD-2

--- src/test/java/irkd/TestCreateIRKD.java
+++ src/test/java/irkd/TestCreateIRKD.java
@@ -66,7 +66,7 @@ public class TestCreateIRKD {
 		// your actual test would go here
 		/////////////////
 		
-		Assert.assertTrue(true);
+		Assert.assertTrue(false);
 	}
 	
 	@Test (groups = {"issue", "integration_tests"})
