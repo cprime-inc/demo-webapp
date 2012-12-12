269cb62d05ee9bf84bd3e4ca673197960958eae4
Sarah Goff-Dupont <sgoffdupont@atlassian.com>
2012-12-12 09:27:41 -0800

    Refactored edit issue classes (IRKD-2). Cleaned up factories, switched calls to use immutable objects when possible. Feels good.

--- src/test/java/irkd/TestCreateIRKD.java
+++ src/test/java/irkd/TestCreateIRKD.java
@@ -58,7 +58,7 @@ public class TestCreateIRKD {
 		// your actual test would go here
 		/////////////////
 		
-		Assert.assertTrue(true);
+		Assert.assertTrue(false);
 	}
 	
 	@Test (groups = {"issue", "integration_tests"})
