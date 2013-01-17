ae4df6d877f6ca2935d5483b210449f214d1cc3c
Sarah Goff-Dupont <sgoffdupont@atlassian.com>
2013-01-17 09:17:37 -0800

    break dependency method

--- src/test/java/irkd/TestBillingAccount.java
+++ src/test/java/irkd/TestBillingAccount.java
@@ -18,7 +18,7 @@ public class TestBillingAccount {
 		// your actual test would go here
 		// ///////////////
 
-		Assert.assertTrue(true);
+		Assert.assertTrue(false);
   }
 
   @Test(groups = "unit", dependsOnMethods = { "testCreateBillingAccount" })
