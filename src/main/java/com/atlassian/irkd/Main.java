7c292c071b82b5b674a5c5f38460718cc92944fd
Tim Pettersen <tim@atlassian.com>
2013-03-19 16:08:44 -0700

    Adding java file with checkstyle violations

---
+++ src/main/java/com/atlassian/irkd/Main.java
@@ -0,0 +1,9 @@
+package com.atlassian.irkd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] array = new String[] {"this","should","be","better","spaced"};
+    }
+    
+}
